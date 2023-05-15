11f369d905c83b2a45195ab9c23f85c2f5c39f9c
Alonso GIRALDO <a.alonso.giraldo@gmail.com>
2023-05-15 21:33:49 +0200

    Change URI path

--- packages/bundles-static/src/constants.js
+++ packages/bundles-static/src/constants.js
@@ -1,9 +1,9 @@
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-const ROOT_PATH = 'bundles-static';
+const ROOT_PATH = 'bundles';
 const BUNDLE_PRODUCT_TYPE = 'static-bundle-parent';
 const MASTER_VARIANT_ID = 1;
-const entryPointUriPath = 'bundles-static';
+const entryPointUriPath = 'bundles';
 
 const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
 
