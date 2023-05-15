e86631592d44d61b027f9d2bc1b0d8c834093399
Alonso GIRALDO <a.alonso.giraldo@gmail.com>
2023-05-15 15:53:26 +0200

    Add environment creation on static bundle

---
+++ packages/platform-extension-static-bundles/configure-environment.sh
@@ -0,0 +1,194 @@
+#!/usr/bin/env bash
+
+# This script is used to configure the CT environment for the application.
+CLIENT_ID="CLIENT_ID"
+CLIENT_SECRET="CLIENT_SECRET"
+REGION="europe-west1.gcp"
+PROJECT_KEY="PROJECT_KEY"
+
+response=$(curl https://auth.${REGION}.commercetools.com/oauth/token \
+     --basic --user "${CLIENT_ID}:${CLIENT_SECRET}" \
+     -X POST \
+     -d "grant_type=client_credentials&scope=manage_project:${PROJECT_KEY} manage_api_clients:${PROJECT_KEY}")
+
+BEARER_TOKEN=$(echo $response | awk -F'"' '/access_token/{print $4}')
+
+curl https://api.${REGION}.commercetools.com/${PROJECT_KEY}/types -i \
+--header "Authorization: Bearer ${BEARER_TOKEN}" \
+--header 'Content-Type: application/json' \
+--data-binary @- << DATA
+{
+  "name": "StaticBundleChildVariant",
+  "description": "The product variant included in a static bundle",
+  "classifier": "Complex",
+  "attributes": [
+    {
+      "name": "quantity",
+      "label": {
+        "en": "Quantity"
+      },
+      "isRequired": true,
+      "type": {
+        "name": "number"
+      },
+      "attributeConstraint": "None",
+      "isSearchable": false,
+      "inputHint": "SingleLine",
+      "displayGroup": "Other"
+    },
+    {
+      "name": "sku",
+      "label": {
+        "en": "SKU"
+      },
+      "isRequired": false,
+      "type": {
+        "name": "text"
+      },
+      "attributeConstraint": "None",
+      "isSearchable": false,
+      "inputHint": "SingleLine",
+      "displayGroup": "Other"
+    },
+    {
+      "name": "variant-id",
+      "label": {
+        "en": "Variant ID"
+      },
+      "isRequired": true,
+      "type": {
+        "name": "number"
+      },
+      "attributeConstraint": "None",
+      "isSearchable": false,
+      "inputHint": "SingleLine",
+      "displayGroup": "Other"
+    },
+    {
+      "name": "product-ref",
+      "label": {
+        "en": "Product"
+      },
+      "isRequired": false,
+      "type": {
+        "name": "reference",
+        "referenceTypeId": "product"
+      },
+      "attributeConstraint": "None",
+      "isSearchable": false,
+      "inputHint": "SingleLine",
+      "displayGroup": "Other"
+    },
+    {
+      "name": "product-name",
+      "label": {
+        "en": "Product Name"
+      },
+      "isRequired": false,
+      "type": {
+        "name": "ltext"
+      },
+      "attributeConstraint": "None",
+      "isSearchable": false,
+      "inputHint": "SingleLine",
+      "displayGroup": "Other"
+    }
+  ],
+  "key": "static-bundle-child-variant"
+}
+DATA
+
+curl https://api.${REGION}.commercetools.com/${PROJECT_KEY}/types -i \
+--header "Authorization: Bearer ${BEARER_TOKEN}" \
+--header 'Content-Type: application/json' \
+--data-binary @- << DATA
+{
+  "name": "StaticBundleParent",
+  "description": "A static bundle of product variants",
+  "classifier": "Complex",
+  "attributes": [
+    {
+      "name": "products",
+      "label": {
+        "en": "Products"
+      },
+      "isRequired": false,
+      "type": {
+        "name": "set",
+        "elementType": {
+          "name": "nested",
+          "typeReference": {
+            "typeId": "product-type",
+            "id": "<ID of static-bundle-child-variant product type goes here>"
+          }
+        }
+      },
+      "attributeConstraint": "None",
+      "isSearchable": true,
+      "inputHint": "SingleLine",
+      "displayGroup": "Other"
+    },
+    {
+      "name": "productSearch",
+      "label": {
+        "en": "Products (Search)"
+      },
+      "isRequired": false,
+      "type": {
+        "name": "set",
+        "elementType": {
+          "name": "text"
+        }
+      },
+      "attributeConstraint": "None",
+      "isSearchable": true,
+      "inputHint": "SingleLine",
+      "displayGroup": "Other"
+    }
+  ],
+  "key": "static-bundle-parent"
+}
+DATA
+
+curl https://api.${REGION}.commercetools.com/${PROJECT_KEY}/types -i \
+--header "Authorization: Bearer ${BEARER_TOKEN}" \
+--header 'Content-Type: application/json' \
+--data-binary @- << DATA
+{
+  "key": "static-bundle-parent-child-link",
+  "name": {
+    "en": "StaticBundleParentChildLink"
+  },
+  "description": {
+    "en": "Link to a parent static bundle product by custom ID"
+  },
+  "resourceTypeIds": [
+    "line-item",
+    "custom-line-item"
+  ],
+  "fieldDefinitions": [
+    {
+      "name": "external-id",
+      "label": {
+        "en": "External ID"
+      },
+      "required": false,
+      "type": {
+        "name": "String"
+      },
+      "inputHint": "SingleLine"
+    },
+    {
+      "name": "parent",
+      "label": {
+        "en": "Parent External ID"
+      },
+      "required": false,
+      "type": {
+        "name": "String"
+      },
+      "inputHint": "SingleLine"
+    }
+  ]
+}
+DATA
